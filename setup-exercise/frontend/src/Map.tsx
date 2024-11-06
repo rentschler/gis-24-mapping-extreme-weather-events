1ab5169f2654b8c18d4dca37cb492198d42177aa
Jonathan Rentschler <jonathan.rentschler@uni-konstanz.de>
2024-11-06 17:23:16 +0100

    add custom popups

--- setup-exercise/frontend/src/Map.tsx
+++ setup-exercise/frontend/src/Map.tsx
@@ -1,10 +1,26 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Circle, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
+import "./map.css";
+type CarSharingPoint = {
+  capacity: number;
+  district: string;
+  latitude: number;
+  longitude: number;
+  name: string;
+  provider: string;
+  address: Address;
+  picture: string;
+};
+
+type Address = {
+  street: string;
+  number: string;
+};
 
 const Map = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<CarSharingPoint[]>([]);
 
   useEffect(() => {
     let isMounted = true;
@@ -26,6 +42,21 @@ const Map = () => {
     };
 }, []);
 
+  const renderPoints = (data: CarSharingPoint[]) => {
+    return data.map((point) => {
+      return (
+        <Marker
+          key={point.name}
+          position={[point.latitude, point.longitude]}
+        >
+          <Popup>
+            <CustomPopup data={point} />
+          </Popup>
+        </Marker>
+      );
+    });
+  }
+
   return (
     <MapContainer
       center={[47.69162, 9.187]}
@@ -36,11 +67,43 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[47.69162, 9.187]}>
-        <Popup>Here is our university!</Popup>
-      </Marker>
-    </MapContainer>
+         { data.length > 0 && renderPoints(data) }
+         {/* red circle at the coordinates of the university */}
+          <Circle
+              center={[47.69162, 9.187]}
+              pathOptions={{ color: 'red' }}
+              radius={10}
+          />
+      </MapContainer>
+
   );
 };
 
 export default Map;
+
+interface CustomPopupProps {
+  data : CarSharingPoint;
+}
+/**
+ * In the popup, you should include the following details about the sharing point:
+• the name of the sharing point,
+• its coordinates,
+• the capacity of cars,
+• the address, and
+• the provider.
+If available, you should also display an image of the sharing point
+ * @param param0 
+ */
+const CustomPopup = ({ data }: CustomPopupProps) => {
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <p>Coordinates: {data.latitude.toFixed(5)}, {data.longitude.toFixed(5)}</p>
+      <p>Capacity: {data.capacity}</p>
+      <p>Address: {data.address.street} {data.address.number}</p>
+      <p>Provider: {data.provider}</p>
+      {data.picture && <img src={data.picture} alt={data.name} />}
+    </div>
+  );
+}
+
