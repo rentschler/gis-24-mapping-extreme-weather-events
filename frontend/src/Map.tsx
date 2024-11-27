eb3b8693bfe4cd4851d7d79d839a64c649af1f49
Wittek, Hubert <hubert.wittek@uni-konstanz.de>
2024-11-27 13:02:35 +0100

    migrated frontend code

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,105 +1,56 @@
-import { useQuery } from "@tanstack/react-query";
+import { MapContainer, Marker, Popup, TileLayer, CircleMarker  } from "react-leaflet";
+import React, { useState, useEffect } from "react";
 
-import axios from "axios";
-import {
-  CircleMarker,
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-} from "react-leaflet";
-import CarSharingPoint from "./CarSharingPoint.ts";
-import { universityOfKonstanzCoordinates } from "./constants.ts";
+
 import "leaflet/dist/leaflet.css";
 
-const fetchSharingPoints = async () => {
-  const { data: sharingPoints } = await axios.get("/api/car-sharing/points");
-
-  return sharingPoints;
-};
-
-const CarSharingPointMarker = ({
-  carSharingPoint,
-}: {
-  carSharingPoint: CarSharingPoint;
-}) => {
-  return (
-    <Marker position={[carSharingPoint.latitude, carSharingPoint.longitude]}>
-      <Popup>
-        <div className="flex flex-col">
-          <h6 className="font-bold text-lg leading-none">
-            {carSharingPoint.name}
-          </h6>
-          <span className="mt-0.5 font-light text-slate-400 italic text-xs">
-            {carSharingPoint.latitude.toFixed(5)},{" "}
-            {carSharingPoint.longitude.toFixed(5)}
-          </span>
-          <div className="flex flex-col">
-            <span className="mt-1 font-bold text-lg">
-              🚘 {carSharingPoint.capacity}
-            </span>
-            <div className="mt-3 flex flex-col">
-              <span>
-                📍 {carSharingPoint.address.street}{" "}
-                {carSharingPoint.address.number}
-              </span>
-              <span>🏢 {carSharingPoint.provider}</span>
-            </div>
-          </div>
-          {carSharingPoint.picture && (
-            <img
-              alt={`A car sharing point in Konstanz called ${carSharingPoint.name}`}
-              className="mt-3"
-              src={carSharingPoint.picture}
-            />
-          )}
-        </div>
-      </Popup>
-    </Marker>
-  );
-};
-
 const Map = () => {
-  const {
-    data: carSharingPoints,
-    error,
-    isError,
-    isPending,
-  } = useQuery<CarSharingPoint[]>({
-    queryKey: ["sharing-points"],
-    queryFn: fetchSharingPoints,
-  });
+  const [points, setPoints] = useState([]);
 
-  if (isError) {
-    console.log(
-      `An error occurred, while fetch the car sharing points: ${error}`,
-    );
-  }
+  useEffect(() => {
+    const fetchPoints = async () => {
+      try {
+        const response = await fetch("/api/data");
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setPoints(data);  // Save fetched data to state
+        } else {
+          console.error("Failed fetching api data:", response);
+        }
+      } catch (error) {
+        console.error("Error fetching api data:", error);
+      }
+    };
+
+    fetchPoints();
+  }, []);
 
   return (
+    <div style={{ height: "500px" }}>
     <MapContainer
-      center={universityOfKonstanzCoordinates}
+      center={[47.69162, 9.187]}
       className="w-screen h-screen"
-      zoom={13}
+      zoom={8}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <CircleMarker
-        center={universityOfKonstanzCoordinates}
-        pathOptions={{ fillColor: "red", fillOpacity: 1, stroke: false }}
-        radius={5}
-      />
-      {!isPending &&
-        carSharingPoints &&
-        carSharingPoints.map((carSharingPoint, index) => (
-          <CarSharingPointMarker
-            carSharingPoint={carSharingPoint}
-            key={index}
-          />
-        ))}
-    </MapContainer>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {points.map((point) => {
+          const latitude = point["LATITUDE"];
+          const longitude = point["LONGITUDE"];
+          return (
+              <CircleMarker
+                center={[latitude, longitude]}
+                radius={2} // Radius of the dot, adjust as needed
+                color="red" // Border color
+                fillColor="red" // Fill color for the dot
+                fillOpacity={1} // Opacity of the fill color
+              ></CircleMarker>
+          );
+        })}
+      </MapContainer>
+    </div>
   );
 };
 
