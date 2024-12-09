5ea96ee528148b1b0e13c75534d0f3b0f933c664
Jonathan Rentschler <jonathan.rentschler@uni-konstanz.de>
2024-12-09 12:07:43 +0100

    add option to the multiselect to allow it  to go over multiple lines

--- frontend/src/pages/settings/SettingsPage.tsx
+++ frontend/src/pages/settings/SettingsPage.tsx
@@ -18,7 +18,7 @@ const SettingsPage = () => {
         showAggregatedEvents,
         showSummaries,
         hideEventsWithoutDescription,
-     } = useSelector((state: RootState) => state.settings);
+    } = useSelector((state: RootState) => state.settings);
 
     const options: SelectProps['options'] = [];
     const defaultValues = [QCLevel.QC1, QCLevel.QC2];
@@ -49,31 +49,41 @@ const SettingsPage = () => {
 
     return (
         <Space direction="vertical" size={18}>
-            <DateRange value={timeRange}
+            <DateRange
+                value={timeRange}
                 onChange={(_: any, dateString: [string, string]) => {
                     // dispatch sends the action to the reducer to update the state
                     dispatch(updateTimeRange(dateString));
-                }} />
-            <Check checked={showPointEvents} 
+                }}
+            />
+            <Check
+                checked={showPointEvents}
                 setChecked={(checked: boolean) => {
                     dispatch(setShowPointEvents(checked));
-                }} 
-                label="Show Point Events" />
-            <Check checked={showAggregatedEvents} 
+                }}
+                label="Show Point Events"
+            />
+            <Check
+                checked={showAggregatedEvents}
                 setChecked={(checked: boolean) => {
                     dispatch(setShowAggregatedEvents(checked));
-                }} 
-                label="Show Aggregated Events" />
-            <Check checked={showSummaries} 
+                }}
+                label="Show Aggregated Events"
+            />
+            <Check
+                checked={showSummaries}
                 setChecked={(checked: boolean) => {
                     dispatch(setShowSummaries(checked));
-                }} 
-                label="Show Summaries" />
-            <Check checked={hideEventsWithoutDescription} 
+                }}
+                label="Show Summaries"
+            />
+            <Check
+                checked={hideEventsWithoutDescription}
                 setChecked={(checked: boolean) => {
                     dispatch(setHideEventsWithoutDescription(checked));
-                }} 
-                label="Hide Events Without Description" />
+                }}
+                label="Hide Events Without Description"
+            />
             <RangeSlider
                 label="Number of Impacts"
                 title="Filter the results based on the Number of Impacts"
@@ -82,12 +92,33 @@ const SettingsPage = () => {
                 max={10}
                 onChange={(value: number[]) => {
                     handleChange(value.map(String));
-                }}>
-            </RangeSlider>
-            <MultiSelect id="impact-multi-select" options={ImpactOptions} defaultValues={ImpactDefaultValues} handleChange={handleChange} placeholder='Select Impact Codes' label='Impact Codes' />
-            <MultiSelect id="qc-multi-select" options={options} defaultValues={defaultValues} handleChange={handleChange} placeholder='Select QC Levels' label='Quality Control Levels' />
-            <MultiSelect id="source-multi-select" options={sourceOptions} defaultValues={sourceDefaultValues} handleChange={handleChange} placeholder='Select Info Sources' label='Info Sources' />
-        </Space>
+                }}
+            ></RangeSlider>
+            <MultiSelect
+                id="impact-multi-select"
+                options={ImpactOptions}
+                defaultValues={ImpactDefaultValues}
+                handleChange={handleChange}
+                placeholder="Select Impact Codes"
+                label="Impact Codes"
+            />
+            <MultiSelect
+                id="qc-multi-select"
+                options={options}
+                defaultValues={defaultValues}
+                handleChange={handleChange}
+                placeholder="Select QC Levels"
+                label="Quality Control Levels"
+                multiLine={true}
+            />
+            <MultiSelect
+                id="source-multi-select"
+                options={sourceOptions}
+                defaultValues={sourceDefaultValues}
+                handleChange={handleChange}
+                placeholder="Select Info Sources"
+                label="Info Sources"
+            /></Space>
     )
 }
 
